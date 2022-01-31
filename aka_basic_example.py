0f0ff3a5d40a0c70c913bc6a9ed3fd7a754b09c6
akamediasystem <nfeehan@gmail.com>
2022-01-31 18:14:31 -0500

    attempting to write ch1 to middle range in init

--- aka_basic_example.py
+++ aka_basic_example.py
@@ -120,9 +120,9 @@ class BasicExample:
         try:
             while 1:
                 if toggle:
-                    tmp[1] = 0x0000
+                    tmp[1] = bytes(0x0000)
                 else:
-                    tmp[1] = 0x3fff
+                    tmp[1] = (bytes(0x3fff)
                 # self._master.slaves[1].output = bytes(tmp)
                 self._master.slaves[1].output = rx_map_obj_bytes
                 # self._master.slaves[1].sdo_write(0x8010, 2, bytes(0x3fff), True)
