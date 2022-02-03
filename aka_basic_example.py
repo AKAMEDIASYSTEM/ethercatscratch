98e3a361a7d3f9f66762a22bc8033611d82dcb76
akamediasystem <nfeehan@gmail.com>
2022-02-03 09:52:35 -0500

    debugging why ch2 not responding, might need to alter sdo

--- aka_basic_example.py
+++ aka_basic_example.py
@@ -123,7 +123,8 @@ class BasicExample:
                     counter = 0x001
                     toggle ^= True
                 rx_map_obj[0] = counter
-                rx_map_obj[1] = max(0x7ffe - counter, 0)
+                # rx_map_obj[1] = max(0x7ffe - counter, 0)
+                rx_map_obj[1] = 0
                 # rx_map_obj[2] = counter
                 rx_map_obj_bytes = struct.pack('Bx' + ''.join(['H' for i in range(len(rx_map_obj))]), len(rx_map_obj), *rx_map_obj)
                 self._master.slaves[1].output = rx_map_obj_bytes
