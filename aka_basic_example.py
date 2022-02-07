3d4f372b81617cf14dc466f0fe089e445efbffef
akamediasystem <nfeehan@gmail.com>
2022-02-07 15:00:00 -0500

    working to get ch1 talking - adding pdo setup transmission now

--- aka_basic_example.py
+++ aka_basic_example.py
@@ -133,7 +133,7 @@ class BasicExample:
                 rx_map_obj[0] = counter
                 # rx_map_obj[1] = max(0x7ffe - counter, 0)
                 # rx_map_obj[1] = 1024
-                rx_map_obj[2] = counter
+                rx_map_obj[1] = counter
                 rx_map_obj_bytes = struct.pack('Bx' + ''.join(['H' for i in range(len(rx_map_obj))]), len(rx_map_obj), *rx_map_obj)
                 self._master.slaves[1].output = rx_map_obj_bytes
                 # print(rx_map_obj)
