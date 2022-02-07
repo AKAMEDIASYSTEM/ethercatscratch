bef145528d05a1a101319cfdd15c774d93d6b329
akamediasystem <nfeehan@gmail.com>
2022-02-07 16:33:26 -0500

    still messing with channel / PDO init

--- aka_basic_example.py
+++ aka_basic_example.py
@@ -133,9 +133,9 @@ class BasicExample:
                     counter = 0x001
                     toggle ^= True
                 rx_map_obj[0] = counter
-                # rx_map_obj[1] = max(0x7ffe - counter, 0)
-                # rx_map_obj[1] = 1024
+                rx_map_obj[1] = max(0x7ffe - counter, 0)
                 rx_map_obj[2] = counter
+                rx_map_obj[3] = max(0x7ffe - counter, 0)
                 tmp = struct.pack('Bx' + ''.join(['H' for i in range(len(rx_map_obj))]), len(rx_map_obj), *rx_map_obj)
                 self._master.slaves[1].output = tmp
                 print(rx_map_obj)
