0b0ad5bccd28def6736a25f7d6f568330b3c4f38
akamediasystem <nfeehan@gmail.com>
2022-02-07 18:25:35 -0500

    testing new module

--- aka_basic_example.py
+++ aka_basic_example.py
@@ -38,7 +38,9 @@ class BasicExample:
         self._expected_slave_layout = {0: SlaveSet('EK1100', self.EK1100_PRODUCT_CODE, None),
                                        # 1: SlaveSet('EL4102', self.EL4102_PRODUCT_CODE, None)
                                        1: SlaveSet('EL4102', self.EL4102_PRODUCT_CODE, self.el4102_setup),
-                                       2: SlaveSet('EL4102', self.EL4102_PRODUCT_CODE, self.el4102_setup)
+                                       2: SlaveSet('EL4102', self.EL4102_PRODUCT_CODE, self.el4102_setup),
+                                       3: SlaveSet('EK1100', self.EK1100_PRODUCT_CODE, None),
+                                       4: SlaveSet('EL4102', self.EL4102_PRODUCT_CODE, self.el4102_setup)
                                        }
 
     def el4102_setup(self, slave_pos):
