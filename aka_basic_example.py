aa35ab27625a57006a7c96f6c572ea0f999fb659
akamediasystem <nfeehan@gmail.com>
2022-02-10 16:54:35 -0500

    still working on ch1

--- aka_basic_example.py
+++ aka_basic_example.py
@@ -66,8 +66,8 @@ class BasicExample:
         # slave.dc_sync(1, 10000000)
         rx_map_obj = [0x1600,
                       0x1601,
-                      0x7010,
-                      0x7020
+                      0x3001,
+                      0x3002
                       ]
         rx_map_obj_bytes = struct.pack(
             'Bx' + ''.join(['H' for i in range(len(rx_map_obj))]), len(rx_map_obj), *rx_map_obj)
