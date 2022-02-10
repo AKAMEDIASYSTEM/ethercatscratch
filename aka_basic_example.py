c750548882d619d1d712aba06fb4fc639881f963
akamediasystem <nfeehan@gmail.com>
2022-02-10 16:21:23 -0500

    still working on ch1

--- aka_basic_example.py
+++ aka_basic_example.py
@@ -135,7 +135,7 @@ class BasicExample:
                     counter = 0x7ffe
                     toggle ^= True
                     print(rx_map_obj)
-                    print(str(tmp))
+                    # print(struct.unpack(tmp))
                 if counter <= 0x0001:
                     counter = 0x001
                     toggle ^= True
@@ -144,7 +144,8 @@ class BasicExample:
                 # rx_map_obj[2] = counter
                 # rx_map_obj[3] = max(0x7ffe - counter, 0)
                 # tmp = struct.pack('Bx' + ''.join(['H' for i in range(len(rx_map_obj))]), len(rx_map_obj), *rx_map_obj)
-                tmp = struct.pack('Bx' + ''.join(['h' for i in range(len(rx_map_obj))]), len(rx_map_obj), *rx_map_obj)
+                # tmp = struct.pack('Bx' + ''.join(['h' for i in range(len(rx_map_obj))]), len(rx_map_obj), *rx_map_obj)
+                tmp = bytearray(rx_map_obj)
                 self._master.slaves[1].output = tmp
                 self._master.slaves[2].output = tmp
                 self._master.slaves[4].output = tmp
