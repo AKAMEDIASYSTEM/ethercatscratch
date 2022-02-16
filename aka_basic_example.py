bedbcf976151346cdeca7ae25ffdd2abd9640d06
akamediasystem <nfeehan@gmail.com>
2022-02-16 16:53:37 -0500

    trying to specify sync modes to see if this is the issue

--- aka_basic_example.py
+++ aka_basic_example.py
@@ -64,11 +64,10 @@ class BasicExample:
         # sending this should 
         # slave.sdo_write(0x1c12, 0, struct.pack('B', 2))
         # slave.dc_sync(1, 10000000)
-        rx_map_obj = [0x1610
-                      ]
+        rx_map_obj = [0x1610]
         rx_map_obj_bytes = struct.pack(
             'Bx' + ''.join(['H' for i in range(len(rx_map_obj))]), len(rx_map_obj), *rx_map_obj)
-        slave.sdo_write(0x1c12, 0, rx_map_obj_bytes, True)
+        slave.sdo_write(0x1c12, 0x1610, rx_map_obj_bytes, True)
         # slave.dc_sync(1, 10000000)
         # slave.dc_sync(1, 1000000)
         print('done setup EL4102')
