3a4259edd677f27e81487ea8db040269c6303732
akamediasystem <nfeehan@gmail.com>
2022-02-17 18:25:06 -0500

    copying startup sdo sends from twincat i think

--- aka_basic_example.py
+++ aka_basic_example.py
@@ -150,10 +150,10 @@ class BasicExample:
                     counter = 0x001
                     toggle ^= True
                     print(rx_map_obj)
-                rx_map_obj[0] = counter
-                rx_map_obj[1] = max(0x7ffe - counter, 0)
-                rx_map_obj[2] = random.randint(0, counter)
-                rx_map_obj[3] = random.randint(0, counter)
+                rx_map_obj[2] = counter
+                rx_map_obj[3] = max(0x7ffe - counter, 0)
+                rx_map_obj[0] = random.randint(0, counter)
+                rx_map_obj[1] = random.randint(0, counter)
                 # tmp = struct.pack('Bx' + ''.join(['H' for i in range(len(rx_map_obj))]), len(rx_map_obj), *rx_map_obj)
                 tmp = struct.pack('Bx' + ''.join(['H' for i in range(len(rx_map_obj))]), len(rx_map_obj), *rx_map_obj)
                 self._master.slaves[1].output = tmp
