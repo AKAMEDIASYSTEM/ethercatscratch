8c5525e3aad62a5f4fc9b827a9ecd46e5480b8d8
akamediasystem <nfeehan@gmail.com>
2022-02-21 15:37:32 -0500

    testing extending el4102_setup

--- aka_basic_example.py
+++ aka_basic_example.py
@@ -37,14 +37,15 @@ class BasicExample:
         self._master.do_check_state = False
         SlaveSet = namedtuple('SlaveSet', 'name product_code config_func')
         self._expected_slave_layout = {0: SlaveSet('EK1100', self.EK1100_PRODUCT_CODE, None),
-                                       1: SlaveSet('EL4102', self.EL4102_PRODUCT_CODE, self.el4102_setup),
-                                       2: SlaveSet('EL4102', self.EL4102_PRODUCT_CODE, self.el4102_setup),
+                                       1: SlaveSet('EL4102', self.EL4102_PRODUCT_CODE, self.el4102_setup(777)),
+                                       2: SlaveSet('EL4102', self.EL4102_PRODUCT_CODE, self.el4102_setup(999)),
                                        3: SlaveSet('EK1100', self.EK1100_PRODUCT_CODE, None),
-                                       4: SlaveSet('EL4102', self.EL4102_PRODUCT_CODE, self.el4102_setup)
+                                       4: SlaveSet('EL4102', self.EL4102_PRODUCT_CODE, self.el4102_setup('foo'))
                                        }
 
-    def el4102_setup(self, slave_pos):
+    def el4102_setup(self, slave_pos, extra):
         slave = self._master.slaves[slave_pos]
+        print(extra)
         # well it turns out no SDO setup is required if we are not changing default behavior!
         print('done setup EL4102')
 
@@ -132,7 +133,6 @@ class BasicExample:
         except KeyboardInterrupt:
             # ctrl-C abort handling
             print('stopped')
-
 
     def run(self):
 
