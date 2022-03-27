c6cce6b8e3f83f73124428e86fcc5d78df8ff61d
akamediasystem <nfeehan@gmail.com>
2022-03-27 15:59:00 +0200

    adding generate-triangle-waves script and updating luts

---
+++ gentri.py
@@ -0,0 +1,19 @@
+# take NUM_SAMPLES samples to generate NUM_CYCLES cycles of triangle wave amplitude AMPLITUDE
+import math
+from numpy import interp
+
+NUM_SAMPLES = 1024
+NUM_CYCLES = 4
+AMPLITUDE = 100
+
+out_lut = [0]*NUM_SAMPLES
+for cycleNumber in range(NUM_CYCLES):
+	print('cycleNumber is {}'.format(cycleNumber))
+	for counter in range(int(NUM_SAMPLES/NUM_CYCLES)):
+		# print('counter is {}'.format(counter))
+		if counter < int(NUM_SAMPLES/NUM_CYCLES)/2:
+			out_lut[(cycleNumber*int(NUM_SAMPLES/NUM_CYCLES)) + counter] = int(interp(counter, [0,int(NUM_SAMPLES/NUM_CYCLES)], [0,2*AMPLITUDE]))
+		else:
+			out_lut[(cycleNumber*int(NUM_SAMPLES/NUM_CYCLES)) + counter] = int(interp(counter, [0,int(NUM_SAMPLES/NUM_CYCLES)], [2*AMPLITUDE,0]))
+
+print(out_lut)
