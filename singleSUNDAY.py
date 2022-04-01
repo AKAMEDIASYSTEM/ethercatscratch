d334bf10ddda8e0661d5d390d5a0b6a6ae1b89b2
akamediasystem <nfeehan@gmail.com>
2022-04-01 12:28:36 +0200

    altering muscle-selection logic

--- singleSUNDAY.py
+++ singleSUNDAY.py
@@ -86,7 +86,7 @@ class BasicExample:
                             muscleCounter = muscleCounter + 1
                             # if (muscleCounter==int(self._muscle)):
                             if (muscleCounter in set_of_muscles):
-                                # logging.debug('muscleCounter MATCH {}'.format(muscleCounter))
+                                logging.debug('muscleCounter MATCH {}'.format(muscleCounter))
                                 output_buffer.append(currentAnimation['lut'][int(max(0, counter - c_phase_offset))])
                                 # logging.debug(output_buffer)
                             else:
