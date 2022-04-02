4718300552a9b44d1a63f63df540347d0d89d5f9
akamediasystem <nfeehan@gmail.com>
2022-04-02 16:52:43 +0200

    adding sigh animation script

--- sigh1.py
+++ sigh1.py
@@ -20,22 +20,28 @@ else:
 	OUT_TIME = 2
 AMPLITUDE = 32766
 '''
+interpolate from mid-to zero slowly, over 2048 seconds
+THEN
 interpolate from mid-amplitude to full aplitude over CHUNK_TIME*IN_TIME cycles
 then interpolate from full aplitude to zero over CHUNK_TIME*OUT_TIME cycles
 then interpolate from zero to mid-amplitude over CHUNK_TIME*(IN_TIME+OUT_TIME) cycles
 
 '''
-
+INITEXHALE_TIME = 1024
 
 out_lut = [0]*(IN_TIME*CHUNK_TIME + OUT_TIME*CHUNK_TIME + CHUNK_TIME*(IN_TIME+OUT_TIME))
-for sampleNumber in range(0,(IN_TIME*CHUNK_TIME)):
-	out_lut[sampleNumber] = int(interp(sampleNumber, [0,(IN_TIME*CHUNK_TIME)], [AMPLITUDE/2,AMPLITUDE]))
 
-for sampleNumber in range((IN_TIME*CHUNK_TIME), ((IN_TIME*CHUNK_TIME) + (OUT_TIME*CHUNK_TIME))):
-	out_lut[sampleNumber] = int(interp(sampleNumber, [(IN_TIME*CHUNK_TIME),((OUT_TIME*CHUNK_TIME)+(IN_TIME*CHUNK_TIME))], [AMPLITUDE, 0]))
+for sampleNumber in range(0,INITEXHALE_TIME):
+	out_lut[sampleNumber] = int(interp(sampleNumber, [0,(INITEXHALE_TIME)], [AMPLITUDE/2,0]))
+
+for sampleNumber in range(INITEXHALE_TIME,(IN_TIME*CHUNK_TIME)):
+	out_lut[sampleNumber] = int(interp(sampleNumber, [INITEXHALE_TIME,INITEXHALE_TIME+(IN_TIME*CHUNK_TIME)], [0,AMPLITUDE]))
+
+for sampleNumber in range((INITEXHALE_TIME+(IN_TIME*CHUNK_TIME)), (INITEXHALE_TIME +(IN_TIME*CHUNK_TIME) + (OUT_TIME*CHUNK_TIME))):
+	out_lut[sampleNumber] = int(interp(sampleNumber, [(INITEXHALE_TIME+(IN_TIME*CHUNK_TIME)),(INITEXHALE_TIME +(IN_TIME*CHUNK_TIME)+(OUT_TIME*CHUNK_TIME))], [AMPLITUDE, 0]))
 
 for sampleNumber in range( ((IN_TIME*CHUNK_TIME) + (OUT_TIME*CHUNK_TIME)),(2*CHUNK_TIME*(IN_TIME+OUT_TIME))):
-	out_lut[sampleNumber] = int(interp(sampleNumber, [((OUT_TIME*CHUNK_TIME)+(IN_TIME*CHUNK_TIME)),2*CHUNK_TIME*(IN_TIME+OUT_TIME)], [0, AMPLITUDE/2]))
+	out_lut[sampleNumber] = int(interp(sampleNumber, [(INITEXHALE_TIME+(OUT_TIME*CHUNK_TIME)+(IN_TIME*CHUNK_TIME)),(INITEXHALE_TIME+2*CHUNK_TIME*(IN_TIME+OUT_TIME))], [0, AMPLITUDE/2]))
 	# print(out_lut[sampleNumber])
 
 # for i, j in enumerate(out_lut):
--- singleSUNDAY.py
+++ singleSUNDAY.py
@@ -106,7 +106,7 @@ class BasicExample:
                     
                 else:
                     # currentAnimation = random.choice(luts.luts)
-                    currentAnimation = luts.luts[10]
+                    currentAnimation = luts.luts[11]
                     logging.debug('chose {}'.format(currentAnimation['name']))
                     MAX_SAMPLES = len(currentAnimation['lut'])
                     currentlyPlaying = True
