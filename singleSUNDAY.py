c08a0fc872e6529701e8d79951c3cbcf38fa9c2b
akamediasystem <nfeehan@gmail.com>
2022-04-02 15:21:35 +0200

    testing power-increase methods for HEAD

--- singleSUNDAY.py
+++ singleSUNDAY.py
@@ -106,7 +106,7 @@ class BasicExample:
                     
                 else:
                     # currentAnimation = random.choice(luts.luts)
-                    currentAnimation = luts.luts[2]
+                    currentAnimation = luts.luts[random.randint(1,2)]
                     logging.debug('chose {}'.format(currentAnimation['name']))
                     MAX_SAMPLES = len(currentAnimation['lut'])
                     currentlyPlaying = True
