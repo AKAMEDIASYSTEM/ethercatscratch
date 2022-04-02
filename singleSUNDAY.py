c30d04236fedaa11d810da87c707194e49782946
akamediasystem <nfeehan@gmail.com>
2022-04-02 17:07:22 +0200

    another try

--- singleSUNDAY.py
+++ singleSUNDAY.py
@@ -106,7 +106,7 @@ class BasicExample:
                     
                 else:
                     # currentAnimation = random.choice(luts.luts)
-                    currentAnimation = luts.luts[11]
+                    currentAnimation = luts.luts[12]
                     logging.debug('chose {}'.format(currentAnimation['name']))
                     MAX_SAMPLES = len(currentAnimation['lut'])
                     currentlyPlaying = True
