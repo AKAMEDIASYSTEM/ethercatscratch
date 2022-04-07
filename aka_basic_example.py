d3c674219bef9f8b82705971a379373aceff45e0
akamediasystem <nfeehan@gmail.com>
2022-04-07 17:10:55 +0200

    trying some propagation offsets at end of luts

--- aka_basic_example.py
+++ aka_basic_example.py
@@ -92,7 +92,7 @@ class BasicExample:
                         counter = 0
                         currentlyPlaying = False
                         self.all_zero()
-                        sleep_interval = random.randint(10)
+                        sleep_interval = random.randint(10,11)
                         logging.debug('sleep for {} seconds'.format(sleep_interval))
                         time.sleep(sleep_interval)
                     
