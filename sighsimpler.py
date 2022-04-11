dd7ca6300e559be950f3973360b69301b98126ed
akamediasystem <nfeehan@gmail.com>
2022-04-11 16:38:41 +0200

    tying back to good note longer exhale

--- sighsimpler.py
+++ sighsimpler.py
@@ -40,7 +40,7 @@ gap after exhale too long
 # note1 good used 5/11 for MAX_EXHALE_RATE
 
 MAX_INHALE_RATE = int(5*AMPLITUDE/9) # 6*AMPLITUDE/11 a little too subtle, 3/5 a little too phaser-y, try 4/7?
-MAX_EXHALE_RATE = int(0.51*AMPLITUDE)
+MAX_EXHALE_RATE = int(0.49*AMPLITUDE)
 
 # INIT_HOLD_START = INIT_EXHALE_DUR
 # INIT_HOLD_END = INIT_EXHALE_DUR + INIT_HOLD_DUR
