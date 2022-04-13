073c03c416bd6dbd19e33582c1dc9d00d24b7a35
akamediasystem <nfeehan@gmail.com>
2022-04-13 10:55:49 +0200

    trying a different animation for long exhale

--- aka_teaching_tree.py
+++ aka_teaching_tree.py
@@ -75,7 +75,7 @@ class BasicExample:
         counter = 0
         currentlyPlaying = False
         shouldAlternate = True
-        set_to_play = [-2, -2, -5, -2, 10] # -4 'sigh_4_6_8_note1_response_this_is_good' is the good one
+        set_to_play = [26] # -4 'sigh_4_6_8_note1_response_this_is_good' is the good one
         play_counter = 0
         try:
             while 1:
